d225367028e50daf99d5810052d613891cddde18
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-02-13 01:17:13 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -13,7 +13,10 @@ def on_press(key):
 def on_release(key):
     pass
 
-n = "Apprehensive_at_Best.mp3"
+m = "Apprehensive_at_Best.mp3"
+w = "Apprehensive_at_Best.wav"
+
+n = w
 
 #a = np.array(read(n)[1])
 #print(a, a.shape)
@@ -25,15 +28,31 @@ sc = pygame.display.set_mode((400, 300))
 pygame.mixer.music.load(n)
 pygame.mixer.music.play()
 
+sou = pygame.mixer.Sound(w)
+
+print(np.array(sou.get_raw()).astype(np.int))
+
 but = None
 
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 listener.start()
 
-while 1:
+while 0:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
+        #elif i.type == pygame.KEYDOWN:
+        #    if i.key == pygame.K_1:
+        #        sou.pause()
+        #        # pygame.mixer.music.stop()
+        #    elif i.key == pygame.K_2:
+        #        sou.unpause()
+        #        # pygame.mixer.music.play()
+        #        sou.set_volume(0.5)
+        #    elif i.key == pygame.K_3:
+        #        sou.unpause()
+        #        # pygame.mixer.music.play()
+        #        sou.set_volume(1)
         elif i.type == pygame.KEYDOWN:
             if i.key == pygame.K_1:
                 pygame.mixer.music.pause()
